7e6d8adb197fd7943fbfcf3edb39743f64baf63d
CristinaVolk <kristinavolk4@tut.by>
2019-12-25 20:44:39 +0100

    Breaking-up the index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<?php include 'includes/head.php'; ?>
+
+<body>
+    <?php include 'includes/header.php' ?>
+    <div id="container">
+        <?php include 'includes/asside.php' ?>  
+        
+      <h1>Home</h1>
+      <p>Just a template.</p>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
