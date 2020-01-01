cebb6cca7b87fc3ea2e451373fa1dad5be6093f9
CristinaVolk <kristinavolk4@tut.by>
2020-01-01 18:50:43 +0100

    Recover process, admin area
    Within the function recover the detection of mode to proceed occurs [created], type of users [added to phpMyAdmin]

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php
+include 'core/init.php';
+protect_page();
+admin_protect();
+include 'includes/overall/over-header.php'; ?>
+        
+      <h1>Admin</h1>
+      <p>Admin page</p>
+
+
+
+<?php include 'includes/overall/over-footer.php'; ?>
